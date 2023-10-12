e9e609717c6e2e2de5c1e58b7a18cb0d03b3f1b7
seajee <teapods@proton.me>
2023-10-12 19:35:10 +0200

    Refactoring; Call connect only once

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import sql from "mysql"
 const DB_COLUMN = process.env.DB_COLUMN;
 const DB_TABLE = process.env.DB_TABLE;
 
-const con = sql.createConnection({
+const db = sql.createConnection({
     // TODO: Define this environment variables
     host: process.env.DB_HOST,       // Currently undefined
     user: process.env.DB_USER,       // Currently undefined
@@ -11,19 +11,20 @@ const con = sql.createConnection({
     database: process.env.DB_NAME    // Currently undefined
 });
 
+db.connect((err) => {
+    if (err) throw err;
+});
+
 function fetchLatestQuote(): string {
     let response: string = "";
 
-    con.connect((err) => {
-        if (err) throw err;
-        con.query(
-            'SELECT ' + DB_COLUMN + ' FROM ' + DB_TABLE + ' DESC LIMIT 0,1',
-            (err, result, _fields) => {
-                if (err) throw err;
-                response = result[0].quote;
-            }
-        );
-    });
+    db.query(
+        ["SELECT", DB_COLUMN, "FROM", DB_TABLE, "DESC LIMIT 0,1"].join(" "),
+        (err, result, _fields) => {
+            if (err) throw err;
+            response = result[0].quote;
+        }
+    );
 
     return response;
 }
@@ -32,16 +33,13 @@ function fetchQuote(id: number): string {
     const idString = id.toString();
     let response: string = "";
 
-    con.connect((err) => {
-        if (err) throw err;
-        con.query(
-            'SELECT ' + DB_COLUMN + ' FROM ' + DB_TABLE + ' WHERE id = ' + idString,
-            (err, result, _fields) => {
-                if (err) throw err;
-                response = result[0].quote;
-            }
-        );
-    });
+    db.query(
+        ["SELECT", DB_COLUMN, "FROM", DB_TABLE, "WHERE id =", idString].join(" "),
+        (err, result, _fields) => {
+            if (err) throw err;
+            response = result[0].quote;
+        }
+    );
 
     return response;
 }
