e0285175ecb125925579ac443d48b4188ca9b316
seajee <teapods@proton.me>
2023-10-13 18:56:47 +0200

    Remove temporary comment

--- src/database.ts
+++ src/database.ts
@@ -52,7 +52,6 @@ function fetchLatestQuote(): string {
     let response: string = "";
 
     db.query(
-        // ["SELECT", DB_COLUMN, "FROM", DB_TABLE, "DESC LIMIT 0,1"].join(" "),
         ["SELECT", "*", "FROM", DB_TABLE, "ORDER BY", "id", "DESC LIMIT 0,1"].join(" "),
         (err, result, _fields) => {
             if (err) {
